5a521dc8d79633213785e96715d959f0a5f9a6ab
Henrik Nesset <Henriknesset@hotmail.com>
2016-04-07 13:42:33 +0200

    1 test split into 2

--- docs/examples/carousel/quiz-1.js
+++ docs/examples/carousel/quiz-1.js
@@ -12,23 +12,8 @@ if (
 !$("input[name=q8]:checked").val() ||            
 !$("input[name=q9]:checked").val() ||            
 !$("input[name=q10]:checked").val() ||            
-!$("input[name=q11]:checked").val() ||            
-!$("input[name=q12]:checked").val() ||            
-!$("input[name=q13]:checked").val() ||            
-!$("input[name=q14]:checked").val() ||            
-!$("input[name=q15]:checked").val() ||            
-!$("input[name=q16]:checked").val() ||            
-!$("input[name=q17]:checked").val() ||            
-!$("input[name=q18]:checked").val() ||            
-!$("input[name=q19]:checked").val() ||            
-!$("input[name=q20]:checked").val() ||            
-!$("input[name=q21]:checked").val() ||            
-!$("input[name=q22]:checked").val() ||            
-!$("input[name=q23]:checked").val() ||            
-!$("input[name=q24]:checked").val() ||            
-!$("input[name=q25]:checked").val() ||            
-!$("input[name=q26]:checked").val() ||            
-!$("input[name=q27]:checked").val()          
+!$("input[name=q11]:checked").val() || 
+!$("input[name=q12]:checked").val()       
 ) {            
 alert("All Quesions need an answear!");        
 }        
@@ -44,23 +29,8 @@ var cat7name = "7";
 var cat8name = "8";            
 var cat9name = "9";            
 var cat10name = "10";            
-var cat11name = "11";            
-var cat12name = "12";            
-var cat13name = "13";            
-var cat14name = "14";            
-var cat15name = "15";            
-var cat16name = "16";            
-var cat17name = "17";            
-var cat18name = "18";            
-var cat19name = "19";            
-var cat20name = "20";            
-var cat21name = "21";            
-var cat22name = "22";            
-var cat23name = "23";            
-var cat24name = "24";            
-var cat25name = "25";            
-var cat26name = "26";            
-var cat27name = "27";                      
+var cat11name = "11"; 
+var cat12name = "12";                      
 var cat28name = "None";            
             
 
@@ -76,21 +46,6 @@ var cat9 = ($("input[name=q9]:checked").val() != "a");
 var cat10 = ($("input[name=q10]:checked").val() != "b");
 var cat11 = ($("input[name=q11]:checked").val() != "a");
 var cat12 = ($("input[name=q12]:checked").val() != "d");
-var cat13 = ($("input[name=q13]:checked").val() != "b"); 
-var cat14 = ($("input[name=q14]:checked").val() != "a"); 
-var cat15 = ($("input[name=q15]:checked").val() != "c"); 
-var cat16 = ($("input[name=q16]:checked").val() != "b"); 
-var cat17 = ($("input[name=q17]:checked").val() != "b"); 
-var cat18 = ($("input[name=q18]:checked").val() != "d"); 
-var cat19 = ($("input[name=q19]:checked").val() != "c"); 
-var cat20 = ($("input[name=q20]:checked").val() != "b"); 
-var cat21 = ($("input[name=q21]:checked").val() != "d"); 
-var cat22 = ($("input[name=q22]:checked").val() != "b"); 
-var cat23 = ($("input[name=q23]:checked").val() != "c"); 
-var cat24 = ($("input[name=q24]:checked").val() != "b"); 
-var cat25 = ($("input[name=q25]:checked").val() != "c"); 
-var cat26 = ($("input[name=q26]:checked").val() != "a"); 
-var cat27 = ($("input[name=q27]:checked").val() != "a");
 
 var cat28 = (!cat1 && !cat2 && !cat3 && 
 			 !cat4 && !cat5 && !cat6 && 
@@ -115,21 +70,6 @@ if (cat9) { categories.push(cat9name) };
 if (cat10) { categories.push(cat10name) };            
 if (cat11) { categories.push(cat11name) };
 if (cat12) { categories.push(cat12name) };
-if (cat13) { categories.push(cat13name) };
-if (cat14) { categories.push(cat14name) };
-if (cat15) { categories.push(cat15name) };
-if (cat16) { categories.push(cat16name) };
-if (cat17) { categories.push(cat17name) };
-if (cat18) { categories.push(cat18name) };
-if (cat19) { categories.push(cat19name) };
-if (cat20) { categories.push(cat20name) };
-if (cat21) { categories.push(cat21name) };
-if (cat22) { categories.push(cat22name) };
-if (cat23) { categories.push(cat23name) };
-if (cat24) { categories.push(cat24name) };
-if (cat25) { categories.push(cat25name) };
-if (cat26) { categories.push(cat26name) };
-if (cat27) { categories.push(cat27name) };
 if (cat28) { categories.push(cat28name) };
 var catStr = 'følgende svar er feil: ' + categories.join(', ') + '';                     
 $("#categorylist").text(catStr);                        
@@ -145,23 +85,8 @@ if (cat7) { $("#category7").show("slow"); };
 if (cat8) { $("#category8").show("slow"); };            
 if (cat9) { $("#category9").show("slow"); };            
 if (cat10) { $("#category10").show("slow"); };            
-if (cat11) { $("#category11").show("slow"); };           
-if (cat12) { $("#category12").show("slow"); };           
-if (cat13) { $("#category13").show("slow"); };           
-if (cat14) { $("#category14").show("slow"); };           
-if (cat15) { $("#category15").show("slow"); };           
-if (cat16) { $("#category16").show("slow"); };           
-if (cat17) { $("#category17").show("slow"); };           
-if (cat18) { $("#category18").show("slow"); };           
-if (cat19) { $("#category19").show("slow"); };           
-if (cat20) { $("#category20").show("slow"); };           
-if (cat21) { $("#category21").show("slow"); };           
-if (cat22) { $("#category22").show("slow"); };           
-if (cat23) { $("#category23").show("slow"); };           
-if (cat24) { $("#category24").show("slow"); };           
-if (cat25) { $("#category25").show("slow"); };           
-if (cat26) { $("#category26").show("slow"); };           
-if (cat27) { $("#category27").show("slow"); };           
+if (cat11) { $("#category11").show("slow"); };   
+if (cat12) { $("#category12").show("slow"); };         
 if (cat28) { $("#category28").show("slow"); };
 { $("#closing").show("slow"); };
 }
